66e74e7b3ea2272fd46a3dbbaecd07a7f45afd24
arfa <arfa@example.com>
2020-01-03 17:30:28 +0700

    koneksi mongodb

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const todoController = require("../controller/todoController");
 
 router.get("/test", todoController.test);
+router.post("/create", todoController.create);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const todos = require("./routes/todoRoutes");
+const mongoose = riquire("mongoose");
 const app = express();
 
+let mongourl = "mongodb://localhost:27017/todoapi";
+
+const mongoDB = mongourl;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.conection;
+db.on("error", console.error.bind(console, "Connection Error, cek koneksi")); 
+
 app.use("/api", todos);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.listen(3000, () => console.log('Server is Running at port : 3000'))
